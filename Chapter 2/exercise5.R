c25edd9063db3992d9178c7bbb24339c1944d97f
Melissa Wong <dispagna3@gmail.com>
2021-01-18 21:13:37 -0500

    checkin Chapter 2 exercises

---
+++ Chapter 2/exercise5.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(ggplot2)
+
+datasets <- data(package = "ggplot2")$results[c(2, 4, 10), "Item"]
+
+ui <- fluidPage(
+  selectInput("dataset", "Dataset", choices = datasets),
+  verbatimTextOutput("summary"),
+  plotOutput("plot")
+)
+
+server <- function(input, output, session) {
+  
+  dataset <- reactive({
+    get(input$dataset, "package:ggplot2")
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  
+  output$plot <- renderPlot({
+    plot(dataset())
+  })
+}
+
+shinyApp(ui, server)
